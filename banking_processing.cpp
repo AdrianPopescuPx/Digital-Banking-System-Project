c08858ffc1cecf583b653028defa5999e853efa3
Adrian Popescu <adrianpopescupx@gmail.com>
2022-11-30 13:37:55 +0200

    added functions & Finished Project

--- banking_processing.cpp
+++ banking_processing.cpp
@@ -117,45 +117,73 @@ void add_money(Account &account, int current_card) {
     cout << "Please enter the amount of money you want to add: ";
     int amount;
     cin >> amount;
-    cout << "\nSuccesfull!\n" << "Your new ballance is: " << (account.cards[current_card].ballance) + amount << "\n\n";
+    account.cards[current_card].ballance += amount;
+    cout << "\nSuccesfull!\n" << "Your new ballance is: " << account.cards[current_card].ballance << "\n\n";
+}
+// 
+void make_loan(Account &account, int current_card) {
+    double loan_amount, monthly_payment, years, amount_paid_back, monthly_interest, number_of_payments;
+
+    cout << "Enter the loan amount (LoanAmount) --> ";
+    cin >> loan_amount;
+    cout << "The International Bank has a fixed interest rate of 5% \n";
+    cout << "Enter the period of time for the loan: \n";
+    cin >> years;
+    number_of_payments = years * 12;
+    monthly_interest = (5 / 100.0) / 12;
+    monthly_payment = monthly_interest * pow (( 1 + monthly_interest ), number_of_payments) / (pow(( 1 + monthly_interest), number_of_payments) -1)  * loan_amount;
+    cout << "Monthly Payment:  " << monthly_payment << endl;
+    amount_paid_back = monthly_payment * number_of_payments;
+    cout << "Amount Paid Back: " << amount_paid_back << endl;
+    cout << "Interest Paid:  " << (amount_paid_back - loan_amount) << endl;
+
+    cout << "Do you want to proceed further with the Load? (Yes / No) ";
+    string decision;
+    cin >> decision;
+    if(decision == "Yes" || decision == "yes" || decision == "YES") {
+        account.cards[current_card].ballance += loan_amount;
+        int second_card;
+        if(current_card == 1)
+            second_card = 0;
+        else 
+            second_card = 1;
+        if(account.cards[second_card].type == "credit") {
+            if(account.cards[second_card].currency == "euro") {
+                account.cards[second_card].ballance *= 4.9;
+                account.cards[current_card].ballance -= account.cards[second_card].ballance;
+                account.cards[second_card].ballance = 0;
+            }
+            else if(account.cards[second_card].currency == "ron") {
+                account.cards[second_card].ballance /= 4.9;
+                account.cards[current_card].ballance -= account.cards[second_card].ballance;
+                account.cards[current_card].ballance -= account.cards[second_card].ballance;
+                account.cards[second_card].ballance = 0;
+            }
+        }
+    }
 }
 
 
-// de facut si un imprumut
-
-
-void make_investment_plan(Account &account, int current_card) {
-    cout << "In order to find your END ballane after the investment you need to provide the following information:\n";
-    cout << "1) Starting Amount: ";
+void economies_account_calculator(Account &account, int current_card) {
+    cout << "Step 1: Initial Investment:\n";
+    cout << "Your current ballance: " << account.cards[current_card].ballance << '\n'; 
+    cout << "Amount of money that you have available to invest initially: ";
     int starting_amount;
     cin >> starting_amount;
-    cout << "How many years of investment?\n";
+    cout << "Length of time, in years, that you plan to save ";
     int years;
     cin >> years;
-    cout << "For your current debit card your Return Rate is " << account.cards[current_card].interest_rate << '\n';
-    cout << "Will you contribute each month or once per year?\n";
-    cout << "I will contribute once per: ";
-    string contribute;
-    cin >> contribute;
-    cout << "Please type the amout additional contribution you want to add each: " << contribute << ' ';
-    int amount;
-    cin >> amount;
-    cout << "Thank you, this are the Results:\n" << "Starting Amount: " << starting_amount << "\n";
-    int total_contributions;
-    if(contribute == "month")
-         total_contributions = ((years * 12) * amount) - amount + starting_amount;
-    else  total_contributions = (years-1) * amount;
-    cout << "Total Contributions: " << total_contributions << '\n';
-
+    cout << "Your interest rate for this account is: " << account.cards[current_card].interest_rate << '\n'; 
+    cout << "In " << years << " years you will have: " << starting_amount * pow((1 + (account.cards[current_card].interest_rate / 100)), years) << '\n';
 }
 
-void manage_account(Account &account, int &current_card) {
+void manage_account(Account &account, int current_card) {
     int option;
     cout << "Please select The operation you want to make:\n";
-    cout << "1) Withdraw Money\n" << "2) Add Money to your balance\n" << "3) Make an investment plan\n\n";
+    cout << "1) Withdraw Money\n" << "2) Add Money to your balance\n" << "3) Economies account calculator\n" << "4) Make a loan\n\n";
     cin >> option;
-    if(option != 1 && option != 2 && option != 3) {
-        while(option != 1 && option != 2 && option != 3) {
+    if(option != 1 && option != 2 && option != 3 && option != 4) {
+        while(option != 1 && option != 2 && option != 3 && option != 4) {
             cout << "Wrong Selection , please select between 1-3\n";
             cin >> option;
         }
@@ -164,45 +192,45 @@ void manage_account(Account &account, int &current_card) {
         withdraw_money(account, current_card);
     else if(option == 2) 
         add_money(account, current_card);
+    else if(option == 3)
+        economies_account_calculator(account, current_card);
     else 
-        make_investment_plan(account, current_card);
+        make_loan(account, current_card);
 }
 
 
 void login_user(Database &database) {
-    cout << "Please enter your card number:\n";
-    string number;
-    cin >> number;
     bool verified = false;
-    for(int i = 0; i < database.number_of_users; ++i) {
+    while(verified == false) {
+        cout << "Please enter your card number:\n";
+        string number;
+        cin >> number;
+        cout << "Please enter your Pin:\n";
+        int entered_pin;
+        cin >> entered_pin;
+        for(int i = 0; i < database.number_of_users; ++i) {
         if(verified)
             break;
         for(int j = 0; j < database.users[i].account.number_of_cards; ++j) {
-            if(database.users[i].account.cards[j].number.find(number) != string::npos) {
+            if(database.users[i].account.cards[j].number.find(number) != string::npos && database.users[i].account.cards[j].pin == entered_pin) {
+                verified = true;
                 cout << "##########   WELCOME BACK   ##########\n";
                 cout << "##########  " << database.users[i].name << "  ##########\n\n";
-                cout << "Please enter your Card Pin Number in order to have acces to your account:\n";
-                int thepin;
-                cin >> thepin;
-                int current_card;
-                while(verified == false) {
-                    if(thepin == database.users[i].account.cards[j].pin) {
-                        verified= true; 
-                        current_card = j;
-                        cout << "Thank you!\n\n";
-                    }
-                    else cout << "Pin incorrect, try again!\n";
-                }
+                int current_card = j;
                 manage_account(database.users[i].account, current_card);
                 break;
+                }
             }
-        };
+        }
+        if(verified == false)
+            cout << "Details Incorrect, please try again!\n";
     }
-
 }
 void new_user_date(date &date);
 
-void read_new_card_data(card &card) {
+void read_new_card_data(card &card, int n) {
+    cout << "Card Number";
+    cout << n << '\n';
     cout << "Debit or Credit Card?\n";
     cin >> card.type;
     cout << "Please select your desired currency\n";
@@ -222,19 +250,28 @@ void read_new_card_data(card &card) {
 
 void new_user_account(Account &account) {
     cout << "How many cards do you want?\n";
+    cout << "Be aware: You can create a maximum of 2 Cards for your account type";
     cin >> account.number_of_cards;
-    cout << "Custom IBAN, please type the IBAN you desire:\n";
-    cin >> account.iban;
+    cout << "Your account IBAN is:\n";
+    account.iban = "RORNCB" + to_string(rng() % 100000000000 + 1000000000);
+    cout << account.iban << '\n';
     for(int i = 1; i <= account.number_of_cards; ++i) {
         card current_card;
-        read_new_card_data(current_card);
+        read_new_card_data(current_card, i);
         account.cards.push_back(current_card);
     }
 }
 
 void new_user_date(date &date) {
     cout << "Please type your Full Birthday\n";
-    cin >> date.day >> date.month >> date.year;
+    cout << "Note that in order to enter a correct Birthday you must type it as follows: XX - day || XX - month || XXXX - year\n";
+    bool correct_date = false;
+    while(correct_date == false) {
+        cin >> date.day >> date.month >> date.year;
+        if(date.day <= 31 && date.month <= 12 && date.year <= 2004)
+            correct_date = true;
+        else cout << "Incorrect Format, try again! \n";
+    }
 }
 
 void read_new_user_data(User &user) {
@@ -247,7 +284,7 @@ void register_user(Database &database) {
 
     User user;
     cout << "Welcome to the registering page!\n\n";
-    cout << "Enter your name: ";
+    cout << "Enter your full name: ";
     
     string name;
     cin.get();
@@ -322,8 +359,8 @@ int main() {
             cout << "Please type YES or NO if you want to make another operation: \n";
             string checker;
             cin >> checker;
-            if(checker == "NO")
-                break; // aici avem o problema
+            if(checker == "NO" || checker == "No" || checker == "no")
+                break;
         }
     }
 
